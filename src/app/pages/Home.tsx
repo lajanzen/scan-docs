e673e770a1391db19748777f0aa10949bf24cc4c
lajanzen <lara-janzen@hotmail.com>
2021-11-10 14:42:20 +0100

    Display recognized text

--- src/app/pages/Home.tsx
+++ src/app/pages/Home.tsx
@@ -1,9 +1,11 @@
 import React, { useState } from 'react';
 import ImageInput from '../components/ImageInput/ImageInput';
 import styles from './Home.module.css';
+import Tesseract from 'tesseract.js';
 
 export default function Home(): JSX.Element {
   const [imgURL, setImgURL] = useState<string | null>(null);
+  const [recognizedText, setRecognizedText] = useState<string | null>(null);
 
   return (
     <div className={styles.container}>
@@ -12,9 +14,27 @@ export default function Home(): JSX.Element {
         src="https://i.ibb.co/cyPZp4J/Notebloc-PDF-Scanner-App-Scan-save-share-logo-removebg-preview.png"
         alt="scan-logo"
       />
-      <p>Turn any document into PDF</p>
-      <button disabled={imgURL !== null}>Scan</button>
-      <ImageInput onImageUpload={setImgURL} />
+      <p>{imgURL ? 'Image has been uploaded' : 'Turn any document into PDF'}</p>
+      <button
+        disabled={imgURL === null}
+        onClick={() => {
+          if (imgURL) {
+            Tesseract.recognize(imgURL, 'eng', {
+              logger: (message) => console.log(message.progress),
+            }).then((result) => {
+              const text = result.data.text;
+              setRecognizedText(text);
+            });
+          }
+        }}
+      >
+        Scan
+      </button>
+      {recognizedText ? (
+        <p>{recognizedText}</p>
+      ) : (
+        <ImageInput onImageUpload={setImgURL} />
+      )}
     </div>
   );
 }
