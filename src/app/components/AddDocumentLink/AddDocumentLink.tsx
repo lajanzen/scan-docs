6a3ee1c730beb48e18f1eab1fda74ee85461f72c
lajanzen <lara-janzen@hotmail.com>
2021-11-16 17:25:25 +0100

    Implement React Router

--- src/app/components/AddDocumentLink/AddDocumentLink.tsx
+++ src/app/components/AddDocumentLink/AddDocumentLink.tsx
@@ -1,9 +1,10 @@
 import React from 'react';
+import { Link } from 'react-router-dom';
 
 export default function AddDocumentLink(): JSX.Element {
   return (
-    <a href="/home">
+    <Link to="/">
       <img src="src/app/assets/plus.svg" />
-    </a>
+    </Link>
   );
 }
--- src/app/main.tsx
+++ src/app/main.tsx
@@ -1,11 +1,19 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import './globals.css';
-import App from './App';
+import { BrowserRouter, Route, Routes } from 'react-router-dom';
+import Documents from './pages/Documents/Documents';
+import Home from './pages/Home/Home';
 
 ReactDOM.render(
   <React.StrictMode>
-    <App />
+    <BrowserRouter>
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="documents" element={<Documents />} />
+      </Routes>
+    </BrowserRouter>
+    ,
   </React.StrictMode>,
   document.querySelector('#root')
 );
